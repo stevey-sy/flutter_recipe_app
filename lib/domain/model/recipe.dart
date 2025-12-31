3eff9b23e1793112f44dbb783a649b990e61535a
stevey-sy <sinsy89@hanmail.net>
2025-12-31 12:50:40 +0900

    모델 추가

---
+++ lib/domain/model/recipe.dart
@@ -0,0 +1,64 @@
+import 'recipe_ingredient.dart';
+
+class Recipe {
+  final int id;
+  final String category;
+  final String name;
+  final String image;
+  final String chef;
+  final String time;
+  final double rating;
+  final List<RecipeIngredient> ingredients;
+
+  Recipe({
+    required this.id,
+    required this.category,
+    required this.name,
+    required this.image,
+    required this.chef,
+    required this.time,
+    required this.rating,
+    required this.ingredients,
+  });
+
+  factory Recipe.fromJson(Map<String, dynamic> json) {
+    return Recipe(
+      id: json['id'] as int,
+      category: json['category'] as String,
+      name: json['name'] as String,
+      image: json['image'] as String,
+      chef: json['chef'] as String,
+      time: json['time'] as String,
+      rating: (json['rating'] as num).toDouble(),
+      ingredients:
+          (json['ingredients'] as List<dynamic>?)
+              ?.map(
+                (item) => RecipeIngredient.fromJson(
+                  item as Map<String, dynamic>,
+                ),
+              )
+              .toList() ??
+          [],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'category': category,
+      'name': name,
+      'image': image,
+      'chef': chef,
+      'time': time,
+      'rating': rating,
+      'ingredients': ingredients
+          .map((item) => item.toJson())
+          .toList(),
+    };
+  }
+
+  @override
+  String toString() {
+    return 'Recipe(id: $id, category: $category, name: $name, image: $image, chef: $chef, time: $time, rating: $rating, ingredients: ${ingredients.length})';
+  }
+}
---
+++ lib/domain/model/recipe_ingredient.dart
@@ -0,0 +1,31 @@
+import 'ingredient.dart';
+
+class RecipeIngredient {
+  final Ingredient ingredient;
+  final int amount;
+
+  RecipeIngredient({
+    required this.ingredient,
+    required this.amount,
+  });
+
+  factory RecipeIngredient.fromJson(Map<String, dynamic> json) {
+    return RecipeIngredient(
+      ingredient: Ingredient.fromJson(json['ingredient'] as Map<String, dynamic>),
+      amount: json['amount'] as int,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'ingredient': ingredient.toJson(),
+      'amount': amount,
+    };
+  }
+
+  @override
+  String toString() {
+    return 'RecipeIngredient(ingredient: $ingredient, amount: $amount)';
+  }
+}
+
